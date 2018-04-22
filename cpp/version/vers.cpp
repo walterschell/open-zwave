24180e8f0bcfd7ad4dc16d125ce98791e6cf383c
Walter Schell <walterschell@users.noreply.github.com>
2018-04-22 10:24:25 -0400

    minozw compiles. No idea if it works

---
+++ cpp/version/vers.cpp
@@ -0,0 +1,5 @@
+#include <Defs.h>
+uint16_t ozw_vers_major = @OpenZWave_VERSION_MAJOR@;
+uint16_t ozw_vers_minor = @OpenZWave_VERSION_MINOR@;
+uint16_t ozw_vers_revision = @OpenZWave_VERSION_PATCH@;
+char ozw_version_string[] = "@OpenZWave_GITVERSION@";
